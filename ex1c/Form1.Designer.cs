235b35fec659473287082e5b9fcfccb597f60977
Krusert-mscs <111943365+Krusert-mscs@users.noreply.github.com>
2023-09-01 21:53:14 -0500

    Food Truck lmao

---
+++ ex1c/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace ex1c
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtOhNo = new System.Windows.Forms.TextBox();
+            this.txtBorgorOfHam = new System.Windows.Forms.TextBox();
+            this.txtHotDiggidyDog = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtITSTHEIRS = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtHotDog = new System.Windows.Forms.TextBox();
+            this.txtBurger = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(268, 227);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(75, 20);
+            this.txtTotal.TabIndex = 10;
+            // 
+            // txtOhNo
+            // 
+            this.txtOhNo.Location = new System.Drawing.Point(268, 128);
+            this.txtOhNo.Name = "txtOhNo";
+            this.txtOhNo.ReadOnly = true;
+            this.txtOhNo.Size = new System.Drawing.Size(75, 20);
+            this.txtOhNo.TabIndex = 8;
+            // 
+            // txtBorgorOfHam
+            // 
+            this.txtBorgorOfHam.Location = new System.Drawing.Point(268, 85);
+            this.txtBorgorOfHam.Name = "txtBorgorOfHam";
+            this.txtBorgorOfHam.ReadOnly = true;
+            this.txtBorgorOfHam.Size = new System.Drawing.Size(75, 20);
+            this.txtBorgorOfHam.TabIndex = 7;
+            // 
+            // txtHotDiggidyDog
+            // 
+            this.txtHotDiggidyDog.Location = new System.Drawing.Point(268, 40);
+            this.txtHotDiggidyDog.Name = "txtHotDiggidyDog";
+            this.txtHotDiggidyDog.ReadOnly = true;
+            this.txtHotDiggidyDog.Size = new System.Drawing.Size(75, 20);
+            this.txtHotDiggidyDog.TabIndex = 6;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(268, 267);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(17, 267);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(201, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Total:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Pretax Total:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "x $5.00 =";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "x $4.00 =";
+            // 
+            // txtITSTHEIRS
+            // 
+            this.txtITSTHEIRS.Location = new System.Drawing.Point(268, 170);
+            this.txtITSTHEIRS.Name = "txtITSTHEIRS";
+            this.txtITSTHEIRS.ReadOnly = true;
+            this.txtITSTHEIRS.Size = new System.Drawing.Size(75, 20);
+            this.txtITSTHEIRS.TabIndex = 9;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(147, 267);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Hot Dogs:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Hamburgers:";
+            // 
+            // txtHotDog
+            // 
+            this.txtHotDog.Location = new System.Drawing.Point(85, 40);
+            this.txtHotDog.Name = "txtHotDog";
+            this.txtHotDog.Size = new System.Drawing.Size(27, 20);
+            this.txtHotDog.TabIndex = 1;
+            this.txtHotDog.Text = "0";
+            // 
+            // txtBurger
+            // 
+            this.txtBurger.Location = new System.Drawing.Point(85, 78);
+            this.txtBurger.Name = "txtBurger";
+            this.txtBurger.Size = new System.Drawing.Size(27, 20);
+            this.txtBurger.TabIndex = 2;
+            this.txtBurger.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(207, 173);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Tax:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 302);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtBurger);
+            this.Controls.Add(this.txtHotDog);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtITSTHEIRS);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtOhNo);
+            this.Controls.Add(this.txtBorgorOfHam);
+            this.Controls.Add(this.txtHotDiggidyDog);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "frmFoodTruck";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtOhNo;
+        private System.Windows.Forms.TextBox txtBorgorOfHam;
+        private System.Windows.Forms.TextBox txtHotDiggidyDog;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtITSTHEIRS;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtHotDog;
+        private System.Windows.Forms.TextBox txtBurger;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ ex1c/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex1c
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            txtHotDiggidyDog.Text =
+                (4.0m * Convert.ToDecimal(txtHotDog.Text)).ToString("0.00");
+            txtBorgorOfHam.Text =
+                (5.0m * Convert.ToDecimal(txtBurger.Text)).ToString("0.00");
+            txtOhNo.Text =
+                (Convert.ToDecimal(txtHotDiggidyDog.Text) + Convert.ToDecimal(txtBorgorOfHam.Text)).ToString("0.00");
+            txtITSTHEIRS.Text =
+               (0.06875m * Convert.ToDecimal(txtOhNo.Text)).ToString("0.00");
+            //(Convert.ToDecimal(txtOhNo) + 1).ToString("0.00");
+
+            txtTotal.Text =
+                (Convert.ToDecimal(txtITSTHEIRS.Text) + Convert.ToDecimal(txtOhNo.Text)).ToString("0.00");
+                
+              //  (Convert.ToDecimal(txtOhNo.Text) + Convert.ToDecimal(txtITSTHEIRS.Text)).ToString("0.00");
+          
+            btnClear.Focus();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtHotDog.Text = "0";
+            txtBurger.Text = "0";
+            txtHotDiggidyDog.Text = "";
+            txtBorgorOfHam.Text = "";
+            txtOhNo.Text = "";
+            txtITSTHEIRS.Text = "";
+            txtTotal.Text = "";
+
+            txtHotDog.Focus();
+        }
+    }
+}
