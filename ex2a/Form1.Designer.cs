632b0c23fab55844042b8c0821259ea9205429e4
Krusert-mscs <111943365+Krusert-mscs@users.noreply.github.com>
2023-09-25 21:12:11 -0500

    ex1a, but 1% more efficient

---
+++ ex2a/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace ex2a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.Subtotal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.DPercent = new System.Windows.Forms.TextBox();
+            this.DAmount = new System.Windows.Forms.TextBox();
+            this.Total = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&Subtotal:";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(46, 198);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 23);
+            this.Calculate.TabIndex = 1;
+            this.Calculate.Text = "&Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Subtotal
+            // 
+            this.Subtotal.Location = new System.Drawing.Point(119, 33);
+            this.Subtotal.Name = "Subtotal";
+            this.Subtotal.Size = new System.Drawing.Size(100, 20);
+            this.Subtotal.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Discount Percent:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Discount Amount:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Total:";
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(144, 198);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 6;
+            this.Exit.Text = "E&xit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // DPercent
+            // 
+            this.DPercent.Location = new System.Drawing.Point(119, 72);
+            this.DPercent.Name = "DPercent";
+            this.DPercent.Size = new System.Drawing.Size(100, 20);
+            this.DPercent.TabIndex = 7;
+            // 
+            // DAmount
+            // 
+            this.DAmount.Location = new System.Drawing.Point(119, 112);
+            this.DAmount.Name = "DAmount";
+            this.DAmount.ReadOnly = true;
+            this.DAmount.Size = new System.Drawing.Size(100, 20);
+            this.DAmount.TabIndex = 8;
+            // 
+            // Total
+            // 
+            this.Total.Location = new System.Drawing.Point(119, 152);
+            this.Total.Name = "Total";
+            this.Total.ReadOnly = true;
+            this.Total.Size = new System.Drawing.Size(100, 20);
+            this.Total.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(231, 239);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.DAmount);
+            this.Controls.Add(this.DPercent);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Subtotal);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ex2a Variables";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.TextBox Subtotal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.TextBox DPercent;
+        private System.Windows.Forms.TextBox DAmount;
+        private System.Windows.Forms.TextBox Total;
+    }
+}
+
