9726f8fb9c0a22cc9abaf845bf460ce6be29b4b8
Krusert-mscs <111943365+Krusert-mscs@users.noreply.github.com>
2023-10-05 14:43:31 -0500

    ex1d but it's not any more efficient

---
+++ ex2b/Form1.Designer.cs
@@ -0,0 +1,417 @@
+﻿
+namespace ex2b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btmReset = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMoneyMoneyMoneyMoneyMoney = new System.Windows.Forms.TextBox();
+            this.txtEquivD = new System.Windows.Forms.TextBox();
+            this.txtRateD = new System.Windows.Forms.TextBox();
+            this.txtAmountD = new System.Windows.Forms.TextBox();
+            this.txtEquivC = new System.Windows.Forms.TextBox();
+            this.txtRateC = new System.Windows.Forms.TextBox();
+            this.txtAmountC = new System.Windows.Forms.TextBox();
+            this.txtEquivB = new System.Windows.Forms.TextBox();
+            this.txtRateB = new System.Windows.Forms.TextBox();
+            this.txtAmountB = new System.Windows.Forms.TextBox();
+            this.txtEquivA = new System.Windows.Forms.TextBox();
+            this.txtRateA = new System.Windows.Forms.TextBox();
+            this.txtAmountA = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(694, 127);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 15);
+            this.label9.TabIndex = 124;
+            this.label9.Text = "US Dollar";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(547, 127);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 15);
+            this.label8.TabIndex = 123;
+            this.label8.Text = "Euro";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(371, 127);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 15);
+            this.label7.TabIndex = 122;
+            this.label7.Text = "Costa Rican Colon";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(218, 127);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 15);
+            this.label6.TabIndex = 121;
+            this.label6.Text = "Bhutanese Ngultrum";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(84, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 15);
+            this.label5.TabIndex = 120;
+            this.label5.Text = "Australian Dollar";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(656, 28);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(137, 96);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 119;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(496, 28);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(137, 96);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 118;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(353, 28);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(137, 96);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 117;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(210, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(137, 96);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 116;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(67, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 115;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(656, 263);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(137, 23);
+            this.btnExit.TabIndex = 113;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btmReset
+            // 
+            this.btmReset.Location = new System.Drawing.Point(496, 263);
+            this.btmReset.Name = "btmReset";
+            this.btmReset.Size = new System.Drawing.Size(137, 23);
+            this.btmReset.TabIndex = 112;
+            this.btmReset.Text = "&Reset";
+            this.btmReset.UseVisualStyleBackColor = true;
+            this.btmReset.Click += new System.EventHandler(this.btmReset_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(639, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 15);
+            this.label4.TabIndex = 114;
+            this.label4.Text = "=";
+            // 
+            // txtMoneyMoneyMoneyMoneyMoney
+            // 
+            this.txtMoneyMoneyMoneyMoneyMoney.Location = new System.Drawing.Point(656, 208);
+            this.txtMoneyMoneyMoneyMoneyMoney.Name = "txtMoneyMoneyMoneyMoneyMoney";
+            this.txtMoneyMoneyMoneyMoneyMoney.ReadOnly = true;
+            this.txtMoneyMoneyMoneyMoneyMoney.Size = new System.Drawing.Size(137, 20);
+            this.txtMoneyMoneyMoneyMoneyMoney.TabIndex = 125;
+            this.txtMoneyMoneyMoneyMoneyMoney.Text = "0.00";
+            // 
+            // txtEquivD
+            // 
+            this.txtEquivD.Location = new System.Drawing.Point(496, 208);
+            this.txtEquivD.Name = "txtEquivD";
+            this.txtEquivD.ReadOnly = true;
+            this.txtEquivD.Size = new System.Drawing.Size(137, 20);
+            this.txtEquivD.TabIndex = 126;
+            this.txtEquivD.Text = "0.00";
+            // 
+            // txtRateD
+            // 
+            this.txtRateD.Location = new System.Drawing.Point(496, 176);
+            this.txtRateD.Name = "txtRateD";
+            this.txtRateD.Size = new System.Drawing.Size(137, 20);
+            this.txtRateD.TabIndex = 111;
+            this.txtRateD.Text = "1.15528";
+            this.txtRateD.TextChanged += new System.EventHandler(this.calculate);
+            // 
+            // txtAmountD
+            // 
+            this.txtAmountD.Location = new System.Drawing.Point(496, 145);
+            this.txtAmountD.Name = "txtAmountD";
+            this.txtAmountD.Size = new System.Drawing.Size(137, 20);
+            this.txtAmountD.TabIndex = 110;
+            this.txtAmountD.Text = "0.00";
+            this.txtAmountD.TextChanged += new System.EventHandler(this.calculate);
+            // 
+            // txtEquivC
+            // 
+            this.txtEquivC.Location = new System.Drawing.Point(353, 208);
+            this.txtEquivC.Name = "txtEquivC";
+            this.txtEquivC.ReadOnly = true;
+            this.txtEquivC.Size = new System.Drawing.Size(137, 20);
+            this.txtEquivC.TabIndex = 127;
+            this.txtEquivC.Text = "0.00";
+            // 
+            // txtRateC
+            // 
+            this.txtRateC.Location = new System.Drawing.Point(353, 176);
+            this.txtRateC.Name = "txtRateC";
+            this.txtRateC.Size = new System.Drawing.Size(137, 20);
+            this.txtRateC.TabIndex = 109;
+            this.txtRateC.Text = "0.00176122";
+            this.txtRateC.TextChanged += new System.EventHandler(this.calculate);
+            // 
+            // txtAmountC
+            // 
+            this.txtAmountC.Location = new System.Drawing.Point(353, 145);
+            this.txtAmountC.Name = "txtAmountC";
+            this.txtAmountC.Size = new System.Drawing.Size(137, 20);
+            this.txtAmountC.TabIndex = 108;
+            this.txtAmountC.Text = "0.00";
+            this.txtAmountC.TextChanged += new System.EventHandler(this.calculate);
+            // 
+            // txtEquivB
+            // 
+            this.txtEquivB.Location = new System.Drawing.Point(210, 208);
+            this.txtEquivB.Name = "txtEquivB";
+            this.txtEquivB.ReadOnly = true;
+            this.txtEquivB.Size = new System.Drawing.Size(137, 20);
+            this.txtEquivB.TabIndex = 128;
+            this.txtEquivB.Text = "0.00";
+            // 
+            // txtRateB
+            // 
+            this.txtRateB.Location = new System.Drawing.Point(210, 176);
+            this.txtRateB.Name = "txtRateB";
+            this.txtRateB.Size = new System.Drawing.Size(137, 20);
+            this.txtRateB.TabIndex = 107;
+            this.txtRateB.Text = "0.0139831";
+            this.txtRateB.TextChanged += new System.EventHandler(this.calculate);
+            // 
+            // txtAmountB
+            // 
+            this.txtAmountB.Location = new System.Drawing.Point(210, 145);
+            this.txtAmountB.Name = "txtAmountB";
+            this.txtAmountB.Size = new System.Drawing.Size(137, 20);
+            this.txtAmountB.TabIndex = 106;
+            this.txtAmountB.Text = "0.00";
+            this.txtAmountB.TextChanged += new System.EventHandler(this.calculate);
+            // 
+            // txtEquivA
+            // 
+            this.txtEquivA.Location = new System.Drawing.Point(67, 208);
+            this.txtEquivA.Name = "txtEquivA";
+            this.txtEquivA.ReadOnly = true;
+            this.txtEquivA.Size = new System.Drawing.Size(137, 20);
+            this.txtEquivA.TabIndex = 129;
+            this.txtEquivA.Text = "0.00";
+            // 
+            // txtRateA
+            // 
+            this.txtRateA.Location = new System.Drawing.Point(67, 176);
+            this.txtRateA.Name = "txtRateA";
+            this.txtRateA.Size = new System.Drawing.Size(137, 20);
+            this.txtRateA.TabIndex = 104;
+            this.txtRateA.Text = "0.717976";
+            this.txtRateA.TextChanged += new System.EventHandler(this.calculate);
+            // 
+            // txtAmountA
+            // 
+            this.txtAmountA.Location = new System.Drawing.Point(67, 145);
+            this.txtAmountA.Name = "txtAmountA";
+            this.txtAmountA.Size = new System.Drawing.Size(137, 20);
+            this.txtAmountA.TabIndex = 102;
+            this.txtAmountA.Text = "0.00";
+            this.txtAmountA.TextChanged += new System.EventHandler(this.calculate);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 15);
+            this.label3.TabIndex = 105;
+            this.label3.Text = "$US:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 103;
+            this.label2.Text = "Rate:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 101;
+            this.label1.Text = "Amount:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 314);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btmReset);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMoneyMoneyMoneyMoneyMoney);
+            this.Controls.Add(this.txtEquivD);
+            this.Controls.Add(this.txtRateD);
+            this.Controls.Add(this.txtAmountD);
+            this.Controls.Add(this.txtEquivC);
+            this.Controls.Add(this.txtRateC);
+            this.Controls.Add(this.txtAmountC);
+            this.Controls.Add(this.txtEquivB);
+            this.Controls.Add(this.txtRateB);
+            this.Controls.Add(this.txtAmountB);
+            this.Controls.Add(this.txtEquivA);
+            this.Controls.Add(this.txtRateA);
+            this.Controls.Add(this.txtAmountA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "vCurrency Exchange";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btmReset;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMoneyMoneyMoneyMoneyMoney;
+        private System.Windows.Forms.TextBox txtEquivD;
+        private System.Windows.Forms.TextBox txtRateD;
+        private System.Windows.Forms.TextBox txtAmountD;
+        private System.Windows.Forms.TextBox txtEquivC;
+        private System.Windows.Forms.TextBox txtRateC;
+        private System.Windows.Forms.TextBox txtAmountC;
+        private System.Windows.Forms.TextBox txtEquivB;
+        private System.Windows.Forms.TextBox txtRateB;
+        private System.Windows.Forms.TextBox txtAmountB;
+        private System.Windows.Forms.TextBox txtEquivA;
+        private System.Windows.Forms.TextBox txtRateA;
+        private System.Windows.Forms.TextBox txtAmountA;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ex2b/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex2b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void calculate(object sender, EventArgs e)
+        {
+            decimal amountAustralia = Convert.ToDecimal(txtAmountA.Text);
+            decimal rateAustralia = Convert.ToDecimal(txtRateA.Text);
+            decimal usdAustralia = amountAustralia * rateAustralia;
+            txtEquivA.Text = usdAustralia.ToString("0.00");
+
+            decimal amountBhutan = Convert.ToDecimal(txtAmountB.Text);
+            decimal rateBhutan = Convert.ToDecimal(txtRateB.Text);
+            decimal usdBhutan = amountBhutan * rateBhutan;
+            txtEquivB.Text = usdBhutan.ToString("0.00");
+
+            decimal amountCoRica = Convert.ToDecimal(txtAmountC.Text);
+            decimal rateCoRica = Convert.ToDecimal(txtRateC.Text);
+            decimal usdCoRica = amountCoRica * rateCoRica;
+            txtEquivC.Text = usdCoRica.ToString("0.00");
+
+            decimal amountEurope = Convert.ToDecimal(txtAmountD.Text);
+            decimal rateEurope = Convert.ToDecimal(txtRateD.Text);
+            decimal usdEurope = amountEurope * rateEurope;
+            txtEquivD.Text = usdCoRica.ToString("0.00");
+
+            decimal totalUSD = usdAustralia + usdBhutan + usdCoRica + usdEurope;
+            
+            txtMoneyMoneyMoneyMoneyMoney.Text = totalUSD.ToString("0.00");
+        }
+
+        private void btmReset_Click(object sender, EventArgs e)
+        {
+            //before you yell at me, this doesn't need variables because the assignment asks to perform calculations using variables, not to reset using variables
+
+            txtAmountA.Text = "0.00";
+            txtAmountB.Text = "0.00";
+            txtAmountC.Text = "0.00";
+            txtAmountD.Text = "0.00";
+            txtRateA.Text = "0.717976";
+            txtRateB.Text = "0.0139831";
+            txtRateC.Text = "0.00176122";
+            txtRateD.Text = "1.15528";
+            txtEquivA.Text = "0.00";
+            txtEquivB.Text = "0.00";
+            txtEquivC.Text = "0.00";
+            txtEquivD.Text = "0.00";
+            txtMoneyMoneyMoneyMoneyMoney.Text = "0.00";
+            txtAmountA.Focus();
+        }
+    }
+}
